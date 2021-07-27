958be843d8c0f537f9dc76b845bffc4703e61636
luca-vogels <luporlp.yt@gmail.com>
2021-07-27 14:02:43 +0200

    Sitemap can handle skips invalid URLs is more compact

--- sitemap.php
+++ sitemap.php
@@ -9,13 +9,14 @@
  * a change frequency (always, hourly, daily, weekly, monthly, yearly, never)
  */
 define('SITEMAP_URLS', array(
-	'home/' => array('1.0', 'monthly'),
-	'about/' => array('0.8', 'yearly')
+
+	'start/' => array('1.0', 'yearly'),
+
 ));
 
 
-/** define here custom '<url>...</url>' entries */
-define('CUSTOM_URL_ENTRIES', '');
+/** define here custom "<url>...</url>" entries */
+define('CUSTOM_URL_ENTRIES', "");
 
 
 /** Duration in seconds how long a generated sitemap should be cached before it gets newly generated */
@@ -35,25 +36,16 @@ if($fmt && (time() + SITEMAP_CACHE_SECONDS) < $fmt){ echo file_get_contents($sit
 $timeFormat = "Y-m-dTH:i:sP";
 $today = date($timeFormat);
 $domain = $_SERVER['SERVER_NAME'];
-$c = '<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
-';
+$c = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" xmlns:xhtml=\"http://www.w3.org/1999/xhtml\">\n";
 foreach(SITEMAP_URLS as $path => $info){
+    $absPath = VIEWS.$path; if(!file_exists($absPath)) continue; // skip non existing URLs
+    $m = filemtime($absPath); $m = ($m ? date($timeFormat, $m) : $today);
     foreach(SUPPORTED_LANGUAGES as $lang){
-        $m = filemtime(VIEWS.$path);
-        $c .= 
-'   <url>
-        <loc>https://'.$domain.'/'.$lang.'/'.$path.'</loc>
-        <lastmod>'.($m ? date($timeFormat, $m) : $today).'</lastmod>
-        <priority>'.$info[0].'</priority>
-        <changefreq>'.$info[1].'</changefreq>
-    </url>
-';
+        $c .= "\t<url>\n\t\t<loc>https://".$domain."/".$lang."/".$path."</loc>\n\t\t<lastmod>".$m."</lastmod>\n";
+        $c .= "\t\t<priority>".$info[0]."</priority>\n\t\t<changefreq>".$info[1]."</changefreq>\n\t</url>\n";
     }
-    $c .= '
-';
 }
-$c .= CUSTOM_URL_ENTRIES.'</urlset>';
+$c .= CUSTOM_URL_ENTRIES."</urlset>";
 file_put_contents('sitemap.xml', $c);
 header('Content-Type: text/xml', true);
 echo $c;
