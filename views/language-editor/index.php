9840bdc64f5b4dca8a9bfa0e3a7f736617c0a745
luca-vogels <luporlp.yt@gmail.com>
2021-07-28 09:41:57 +0200

    Language editor updates globals.json on create/delete language

--- views/language-editor/index.php
+++ views/language-editor/index.php
@@ -9,21 +9,54 @@
             if(substr($k, 0, 4) !== 'txt-') continue;
             $json[substr($k, 4)] = $v;
         }
+        ksort($json); // sort by key
         $saveJson = json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
         if($saveJson) file_put_contents(TRANSLATIONS.$selectedLang.'.json', $saveJson);
     }
 
     // create new language
     else if(isset($_POST['new-lang']) && !empty($_POST['new-lang'])){
+        $lang = strtolower($_POST['new-lang']);
+        $LANG = strtoupper($lang);
+        $SEL_LANG = strtoupper($selectedLang); $SEL_LANG_LEN = strlen($SEL_LANG);
         if($selectedLang === 'globals') $selectedLang = DEFAULT_LANGUAGE_CODE;
+        // duplicate language file
         $content = file_get_contents(TRANSLATIONS.$selectedLang.'.json');
-        if($content) file_put_contents(TRANSLATIONS.strtolower($_POST['new-lang']).'.json', $content);
+        if($content) file_put_contents(TRANSLATIONS.$lang.'.json', $content);
+        // update globals.json
+        $inputJson = json_decode(file_get_contents(TRANSLATIONS.'globals.json'), true);
+        if($inputJson){
+            $outputJson = array();
+            foreach($inputJson as $k=>$v){
+                $outputJson[$k] = $v;
+                if(str_ends_with($k, $SEL_LANG)){
+                    $nk = substr($k, 0, -$SEL_LANG_LEN).$LANG;
+                    if(!array_key_exists($nk, $inputJson)) $outputJson[$nk] = $v;
+                }
+            }
+            ksort($outputJson); // sort by key
+            $saveJson = json_encode($outputJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+            if($saveJson) file_put_contents(TRANSLATIONS.'globals.json', $saveJson);
+        }
         header("Location: ?l=".$_POST['new-lang']); exit(0);
     }
 
     // delete language
     else if(isset($_POST['delete'])){
-        if($selectedLang !== 'globals') unlink(TRANSLATIONS.$selectedLang.'.json');
+        if($selectedLang !== 'globals'){
+            // delete language file
+            unlink(TRANSLATIONS.$selectedLang.'.json');
+            // update globals.json
+            $SEL_LANG = strtoupper($selectedLang);
+            $inputJson = json_decode(file_get_contents(TRANSLATIONS.'globals.json'), true);
+            if($inputJson){
+                $outputJson = array();
+                foreach($inputJson as $k=>$v) if(!str_ends_with($k, $SEL_LANG)) $outputJson[$k] = $v;
+                ksort($outputJson); // sort by key
+                $saveJson = json_encode($outputJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+                if($saveJson) file_put_contents(TRANSLATIONS.'globals.json', $saveJson);
+            }
+        }
         header("Location: ?"); exit(0);
     }
 
